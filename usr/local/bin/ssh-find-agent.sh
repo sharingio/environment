8f10f162207dab52b05820c545523858dca854fd
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-11-09 10:59:33 +1300

    Fix path for ssh-find-agent.sh

--- usr/local/bin/ssh-find-agent.sh
+++ usr/local/bin/ssh-find-agent.sh
@@ -7,8 +7,8 @@ if [ $_ = $0 ]; then
 fi
 
 if [ -n "$(find /tmp /run/host/tmp -maxdepth 1 -name 'ssh-*' -print -quit 2> /dev/null)" ] ; then    
-    sudo chgrp -Rf users {run/host,}/tmp/ssh-* 2> /dev/null
-    sudo chmod -Rf 0770 {run/host,}/tmp/ssh-* 2> /dev/null
+    sudo chgrp -Rf users {/run/host,}/tmp/ssh-* 2> /dev/null
+    sudo chmod -Rf 0770 {/run/host,}/tmp/ssh-* 2> /dev/null
     export SSH_AUTH_SOCK=$(find /tmp /run/host/tmp/ \
         -type s -regex '.*/ssh-.*/agent..*$' -printf '%T@ %p\n' 2> /dev/null \
         | sort --numeric-sort | tail -n 1 | awk '{print $2}')
