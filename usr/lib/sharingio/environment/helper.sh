5ae5400287a22e471ceaa05081b9de6dd2b9ad02
Caleb Woodbine <calebwoodbine.public@gmail.com>
2021-11-09 15:45:14 +1300

    Update arch helper

--- usr/lib/sharingio/environment/helper.sh
+++ usr/lib/sharingio/environment/helper.sh
@@ -5,27 +5,35 @@
 #   - x86_64
 #   - arm64
 
-ARCH=$(uname -m)
-export ARCH_TYPE_1="${ARCH}"
+ARCH="$(uname -m)"
+if [ "${ARCH}" = "aarch64" ]; then
+    export ARCH_TYPE_1="arm64"
+else
+    export ARCH_TYPE_1="${ARCH}"
+fi
 
 # ARCH_TYPE_2
 # examples:
 #   - amd64
 #   - arm64
 
-ARCH=$(uname -m)
+ARCH="$(uname -m)"
 if [ "${ARCH}" = "x86_64" ]; then
     export ARCH_TYPE_2="amd64"
+elif [ "${ARCH}" = "aarch64" ]; then
+    export ARCH_TYPE_2="arm64"
 else
-    export ARCH_TYPE_2=${ARCH}
+    export ARCH_TYPE_2="${ARCH}"
 fi
 
 # ARCH_TYPE_3
-ARCH=$(uname -m)
+ARCH="$(uname -m)"
 if [ "${ARCH}" = "arm64" ]; then
+    export ARCH_TYPE_3="arm64v8"
+elif [ "${ARCH}" = "aarch64" ]; then
     export ARCH_TYPE_3="arm64v8"
 elif [ "${ARCH}" = "x86_64" ]; then
     export ARCH_TYPE_3="amd64"
 else
-    export ARCH_TYPE_3=${ARCH}
+    export ARCH_TYPE_3="${ARCH}"
 fi
